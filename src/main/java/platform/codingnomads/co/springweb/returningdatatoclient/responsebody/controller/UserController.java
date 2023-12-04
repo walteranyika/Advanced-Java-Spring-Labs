4f9f945033cecdf281fce4f03e91b75979a54660
Walter <walter@tradesy.com>
2023-12-04 08:05:54 +0300

    Added user-list api endpoint

--- src/main/java/platform/codingnomads/co/springweb/returningdatatoclient/responsebody/controller/UserController.java
+++ src/main/java/platform/codingnomads/co/springweb/returningdatatoclient/responsebody/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import platform.codingnomads.co.springweb.returningdatatoclient.responsebody.model.User;
 
+import java.util.List;
+
 @Controller
 public class UserController {
 
@@ -34,6 +36,15 @@ public class UserController {
     @GetMapping("/user")
     public User user() {
         return user;
+    }
+
+    @ResponseBody
+    @GetMapping("/user-list")
+    public List<User> user_list(){
+        User user1 = User.builder().email("tom@example.com").name("Tom Heaton").build();
+        User user2 = User.builder().email("scarlet@example.com").name("Scarlet").build();
+        User user3 = User.builder().email("simpson@example.com").name("Simpson").build();
+        return  List.of(user1, user2, user3);
     }
 
 }

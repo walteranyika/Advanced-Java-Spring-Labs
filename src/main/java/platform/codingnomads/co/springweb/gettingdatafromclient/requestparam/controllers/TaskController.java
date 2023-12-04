698a0a3c261067b8389c69d3a881167b3171ca96
Walter <walter@tradesy.com>
2023-12-04 09:12:16 +0300

    Added extra api endpoints

--- src/main/java/platform/codingnomads/co/springweb/gettingdatafromclient/requestparam/controllers/TaskController.java
+++ src/main/java/platform/codingnomads/co/springweb/gettingdatafromclient/requestparam/controllers/TaskController.java
@@ -14,6 +14,7 @@ import java.util.stream.IntStream;
 @RequiredArgsConstructor
 public class TaskController {
 
+
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public String getTask(@RequestParam Long id) {
         return "ID: " + id;
@@ -26,10 +27,9 @@ public class TaskController {
 
     @GetMapping(value = "/request-param-optional", produces = MediaType.APPLICATION_JSON_VALUE)
     public Task getTaskWithOptionalRequestPram(@RequestParam(name = "id", required = false) Long taskId) {
-        if (taskId != null){
+        if (taskId != null) {
             return Task.builder().id(taskId).name("Task One").build();
-        }
-        else
+        } else
             return Task.builder().name("Task One").build();
     }
 
@@ -43,5 +43,24 @@ public class TaskController {
         return IntStream.range(0, names.size())
                 .mapToObj(i -> Task.builder().id((long) i).name(names.get(i)).build())
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("/search")
+    public List<Task> searchTasks(@RequestParam(name = "keyword", defaultValue = "", required = false) String keyword) {
+        return tasks().stream().filter(t -> t.getName().toLowerCase().contains(keyword))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/status")
+    public  List<Task> tasksByStatus(@RequestParam boolean completed){
+        return  tasks().stream().filter(t->t.getCompleted()==completed).collect(Collectors.toList());
+    }
+    private List<Task> tasks() {
+        var task1 = Task.builder().id(1L).name("Read Magazine").completed(true).build();
+        var task2 = Task.builder().id(2L).name("Exercise").completed(false).build();
+        var task3 = Task.builder().id(3L).name("Cook Lunch").completed(false).build();
+        var task4 = Task.builder().id(4L).name("Run Marathon").completed(true).build();
+        var task5 = Task.builder().id(5L).name("Watch Football").completed(false).build();
+        return List.of(task1, task2, task3, task4, task5);
     }
 }
